6f664911066185e65cb47de01a33c9f695488c2f
rayhanzidaneachmad <rayhanachmad@student.unmul.ac.id>
2022-04-18 00:10:35 +0800

    postest3_Rayhan Zidane Achmad_1915016049

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: new Haldua(),
+    title: "Navigasi",
+    routes: <String, WidgetBuilder>{
+      '/MyApp':(BuildContext context) => new MyApp(),
+      '/Haldua':(BuildContext context) => new Haldua(),
+    },
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -50,31 +57,148 @@ class _ListDataState extends State<ListData> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return new Scaffold(
+      body: new Center(
       child: ListView.builder(itemBuilder: (BuildContext context, int index){
         final number = index + 1;
         final name = names[index].toString();
-        return Card(
-          child: Padding(
+        return new Card(
+          child: new Padding(
             
             padding: const EdgeInsets.all(15.0),
             
-            child: Row(
+            child: new Row(
               
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Image.asset(
+                 Image.asset(
                   'images/_$number.JPG', width: 150,
                 ),
                 Text(name),
                 Icon(Icons.paid),
                 Text("Rp 50.000"),
                 Icon(Icons.launch),
+                Container(
+                  child: new IconButton(
+                    icon: new Icon(Icons.launch, size: 50.0,),
+                    onPressed: (){
+                      Navigator.pushNamed(context, '/Haldua');
+                    },
+                  ),
+                )
                 ],
             ),
           ),
         );
       },itemCount: names.length,),
+    ),
+    );
+  }
+}
+
+class Haldua extends StatefulWidget {
+  const Haldua({ Key? key }) : super(key: key);
+
+  @override
+  State<Haldua> createState() => _HalduaState();
+}
+
+enum Gender { mahasiswa, bekerja }
+
+class _HalduaState extends State<Haldua> {
+  String namaDepan = "-", namaBelakang = "-";
+
+  final namaDepanController = TextEditingController();
+  final namaBelakangController = TextEditingController();
+  bool? isActive = false;
+  Gender? status = Gender.mahasiswa;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+
+    namaDepanController.dispose();
+    namaBelakangController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("formulir"),
+      ),
+      body: ListView(
+        children: [
+          Text(
+            "Nama saya: ${namaDepan} ${namaBelakang}",
+            style: TextStyle(fontSize: 30),
+          ),
+          Divider(),
+          TextField(
+            controller: namaDepanController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: "masukkan nama",
+            ),
+          ),
+          SizedBox(height: 20),
+          TextFormField(
+            maxLines: 4,
+            controller: namaBelakangController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Nama belakang",
+            ),
+          ),
+          ListTile(
+            leading: Radio(
+              value: Gender.mahasiswa,
+              groupValue: status,
+              onChanged: (Gender? value) {
+                setState(() {
+                  status = value;
+                });
+              },
+            ),
+            title: Text("Mahasiswa"),
+          ),
+          ListTile(
+            leading: Radio(
+              value: Gender.bekerja,
+              groupValue: status,
+              onChanged: (Gender? value) {
+                setState(() {
+                  status = value;
+                });
+              },
+            ),
+            title: Text("Bekerja"),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Checkbox(
+                value: isActive,
+                onChanged: (value) {
+                  setState(() {
+                    isActive = value;
+                  });
+                },
+              ),
+            ],
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                namaDepan = namaDepanController.text;
+                namaBelakang = namaBelakangController.text;
+              });
+            },
+            child: Text("Tampilkan Output"),
+          ),
+        ],
+      ),
     );
   }
 }
